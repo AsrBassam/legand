4b3543b39416109a3a2cd8b5958e5e101a88782c
AsrBassam <45674276+AsrBassam@users.noreply.github.com>
2019-02-14 20:54:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3262,7 +3262,24 @@ console.log(`Back Online In ${client.guilds.size} Servers!`);
 });
 
 
-
+if (message.content.startsWith(adminprefix + 'play')) {//fras
+  client.user.setGame(argresult);//fras
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)//fras
+} else //fras
+  if (message.content.startsWith(adminprefix + 'name')) {//fras
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'avatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'streaming')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Red-Army");//wennnn
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+});
 
 
 	
